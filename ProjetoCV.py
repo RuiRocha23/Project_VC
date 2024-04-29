a1de296f182fbf4f7a401ea3b85a70b6c37e82a4
RuiRocha23 <96590570+RuiRocha23@users.noreply.github.com>
2024-04-29 11:33:39 +0100

    UI

--- ProjetoCV.py
+++ ProjetoCV.py
@@ -6,12 +6,11 @@ cap = cv2.VideoCapture(0)
 def trackbarcallback(value):
     #print(value)
     pass
-    
 
 cv2.namedWindow('trackbar')
 cv2.createTrackbar('par1','trackbar',90,100,trackbarcallback)
 cv2.createTrackbar('par2','trackbar',60,100,trackbarcallback)
-cv2.createTrackbar('thresh','trackbar',1,500,trackbarcallback)
+cv2.createTrackbar('thresh','trackbar',101,500,trackbarcallback)
 
 
 def trackbar_values():
@@ -41,7 +40,7 @@ def check_dirty(image, circle,x1,y1,x2,y2,threshold):
 
     cv2.imwrite(f"Captura.png",resized_img)
     #thresh=cv2.adaptiveThreshold(resized_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, (threshold*2+1), c)
-    _, thresh = cv2.threshold(resized_img, threshold, 255, cv2.THRESH_BINARY) # Threshold para identificar pixeis diferentes de branco
+    _, thresh = cv2.threshold(resized_img, threshold, 255, cv2.THRESH_BINARY)           # Threshold para identificar pixeis diferentes de branco
     cv2.imwrite(f"Thresh.png",thresh)
     non_white_pixels = thresh.size - cv2.countNonZero(thresh)                         # Conta numero de pixeis pretos
     print(non_white_pixels)
@@ -50,18 +49,43 @@ def check_dirty(image, circle,x1,y1,x2,y2,threshold):
     else:
         return False
 
+def text_feedback(feedback):
+    new_x1, new_y1 = x1, y1 - 30
+    new_x2, new_y2 = x2, y1
+    text = feedback
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.8
+    thickness = 2
+    text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
+    text_x = new_x1 + (new_x2 - new_x1 - text_size[0]) // 2
+    text_y = new_y1 + (new_y2 - new_y1 + text_size[1]) // 2
+    cv2.rectangle(frame2, (new_x1, new_y1), (new_x2, new_y2), (255, 0, 0), -1)
+    cv2.putText(frame2, text, (text_x, text_y), font, font_scale, (255, 255, 255), thickness)
+
+def amount_dirty_plates(dirty_plates):
+    text = "Amount of dirty plates: {}".format(dirty_plates)
+    font_scale = 0.5
+    thickness = 1
+    (text_width, text_height), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness)
+    padding = 10
+    text_x = padding
+    text_y = text_height + padding
+    color = (255, 0, 0)  
+    cv2.putText(frame2, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, thickness)
+
+
 
 
 while cv2.waitKey(1) != ord('q'):
     ret, frame = cap.read()
     ret, frame2= cap.read()
     par_1 ,par_2,threshold= trackbar_values()
-
     gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
     detected_circles = cv2.HoughCircles(blurred_image, cv2.HOUGH_GRADIENT, 1, 50, param1 = par_1, param2 = par_2, minRadius = 0, maxRadius = 100)
 
     if detected_circles is not None:
+        dirty_plates=0
         detected_circles = np.uint16(np.around(detected_circles))
 
         for pt in detected_circles[0, :]:
@@ -70,36 +94,19 @@ while cv2.waitKey(1) != ord('q'):
             x2, y2 = x + r, y + r
             is_dirty = check_dirty(frame,pt,x1,y1,x2,y2,threshold)
             if is_dirty:
-                new_x1, new_y1 = x1, y1 - 30
-                new_x2, new_y2 = x2, y1
-                text = "Dirty"
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                font_scale = 0.8
-                thickness = 2
-                text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
-                text_x = new_x1 + (new_x2 - new_x1 - text_size[0]) // 2
-                text_y = new_y1 + (new_y2 - new_y1 + text_size[1]) // 2
-                cv2.rectangle(frame2, (new_x1, new_y1), (new_x2, new_y2), (255, 0, 0), -1)
-                cv2.putText(frame2, text, (text_x, text_y), font, font_scale, (255, 255, 255), thickness)
+                dirty_plates+=1
+                text_feedback("Dirty")
             else:
-                new_x1, new_y1 = x1, y1 - 30
-                new_x2, new_y2 = x2, y1
-                text = "Clean"
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                font_scale = 0.8
-                thickness = 2
-                text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
-                text_x = new_x1 + (new_x2 - new_x1 - text_size[0]) // 2
-                text_y = new_y1 + (new_y2 - new_y1 + text_size[1]) // 2
-                cv2.rectangle(frame2, (new_x1, new_y1), (new_x2, new_y2), (255, 0, 0), -1)
-                cv2.putText(frame2, text, (text_x, text_y), font, font_scale, (255, 255, 255), thickness)
-
-            #print(is_dirty)
+                text_feedback("Clean")
+        
             cv2.rectangle(frame2, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+        amount_dirty_plates(dirty_plates)
+        dirty_plates=0
         print(len(detected_circles[0,:]))
 
     cv2.imshow("Detected Circle", frame2) 
-    #cv2.imshow("canny",canny_image)
+
 
 
 cap.release()
